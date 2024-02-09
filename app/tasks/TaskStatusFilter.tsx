6a553f09b9f869129b29ceb96f68347b2be7004e
toufiq-dev <toufiq.dev@gmail.com>
2024-02-09 23:08:29 +0600

    Pagination Layout

--- app/tasks/TaskStatusFilter.tsx
+++ app/tasks/TaskStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: { label: string; value?: Status }[] = [
   { label: "All" },
@@ -13,12 +13,19 @@ const statuses: { label: string; value?: Status }[] = [
 
 const TaskStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
-        const query = status === "ALL" ? "" : `?status=${status}`;
-        router.push(`/tasks/${query}`);
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = params.size ? "?" + params.toString() : "";
+        router.push("/tasks" + query);
       }}
     >
       <Select.Trigger placeholder="Filter by status..." />
--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -59,7 +59,10 @@ const TasksPage = async ({ searchParams }: Props) => {
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
                 <NextLink
                   href={{
                     query: {
